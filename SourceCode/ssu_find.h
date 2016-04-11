cb3fe430a4adcd6d13d848dd9aadcc21be4f859c
lika <likalella95@gmail.com>
2016-04-12 02:14:23 +0900

    add struct findSignal. (to manage -type option)

--- SourceCode/ssu_find.h
+++ SourceCode/ssu_find.h
@@ -1,4 +1,15 @@
 #ifndef SSU_FIND_H
 #define SSU_FIND_H
 
+// struct findSignal has [TYPE] info.
+struct findSignal{
+     int is_f;
+     int is_d;
+     int is_b;
+     int is_c;
+     int is_p;
+     int is_l;
+     int is_s;
+};
+
 #endif
